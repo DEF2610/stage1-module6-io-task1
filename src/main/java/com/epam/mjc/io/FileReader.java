1006fe5841218b24dac8b1e09806e3074d64db46
d <maksimukart@mail.com>
2023-02-19 17:43:11 +0100

    dd

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,25 +5,22 @@ import java.util.HashMap;
 
 public class FileReader {
 
-    Profile profile = new Profile();
-
     static String directory = System.getProperty("user.dir");
     static String fileName = "Profile.txt";
     static String path = directory + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + fileName;
 
+    static File file = new File(path);
     static HashMap<String, String> lineMap = new HashMap<>();
 
     public Profile getDataFromFile(File file) {
-        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(path))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 parse(line);
                 line = bufferedReader.readLine();
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
         } catch (IOException e) {
-            System.out.println("Exception IO");
+            e.printStackTrace();
         }
         return new Profile();
     }
@@ -33,18 +30,12 @@ public class FileReader {
         lineMap.put(strings[0], strings[1]);
     }
 
-
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        File file = new File(path);
-
         Profile profile = fileReader.getDataFromFile(file);
-
         profile.setName(lineMap.get("Name:"));
         profile.setEmail(lineMap.get("Email:"));
         profile.setAge(Integer.parseInt(lineMap.get("Age:")));
         profile.setPhone(Long.parseLong(lineMap.get("Phone:")));
-
-        System.out.println(profile);
     }
 }
